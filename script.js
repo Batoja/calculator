082c83308d1ff3b5f2051f07aad5f8e8f191352d
Damian Czerniakiewicz <damianczerniakiewicz@damians-macbook-pro.home>
2023-01-02 19:49:30 +0100

    fixed the updateDisplay error and added rest of the functions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 class Calculator {
-    constructor(prevousOperandTextElement, currentOperandTextElement){
+    constructor(previousOperandTextElement, currentOperandTextElement){
         this.previousOperandTextElement = previousOperandTextElement
-        this.currentOperantTextElement = currentOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
         this.clear()
     }
 
@@ -12,18 +12,56 @@ clear (){
 }
 
 
-delete(){}
+delete(){
+    this.currentOperand = this.currentOperand.toString().slice(0, -1)
 
-appendNumber(number){
-    this.currentOperand = number
 }
 
-chooseOperation(operation){}
+appendNumber(number){
+    if (number === "." && this.currentOperand.includes(".")) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+}
 
-compute(){}
+chooseOperation(operation){
+    if (this.currentOperand === "") return
+    if (this.previousOperand !== "") {
+        this.compute()
+    }
+    this.operation = operation
+    this.previousOperand = this.currentOperand
+    this.currentOperand = ""
+}
+
+compute(){
+    let computation
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(current)) return
+    switch (this.operation) {
+        case "+":
+            computation = prev + current
+            break
+        case "-":
+            computation = prev - current
+            break
+        case "+":
+            computation = prev / current
+            break
+        case "*":
+            computation = prev * current
+            break 
+        default:
+            return
+    }
+    this.currentOperand = computation
+    this.operation = undefined
+    this.previousOperand = ""
+}
 
 updateDisplay(){
     this.currentOperandTextElement.innerText = this.currentOperand
+    if (this.operation !== null)
+    this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`
 }
 }
 
@@ -43,4 +81,26 @@ numberButtons.forEach(button => {
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
     })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener("click", button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener("click", button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener("click", button => {
+    calculator.delete()
+    calculator.updateDisplay()
 })
